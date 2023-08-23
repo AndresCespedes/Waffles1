94eedd81f3deb65ccaa900c010c185a24d731d0d
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-23 18:39:20 -0500

    Errase the metadada

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,6 @@ import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Hernández Asesoría en Tránsito',
-  description: 'Es una web para asesorías y tramites de transito',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
