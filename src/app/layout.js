db37601e0cade693b4f98ba39848ded72a946e5a
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-23 18:45:20 -0500

    fix tje imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 "use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
-import NavBar from '@/components/NavBar'
-import Footer from '@/components/Footer'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
