b656dd1eee590ca505c17c53e3b894bd8888de69
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-23 19:10:04 -0500

    Fix the imports in navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 "use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
-import NavBar from '../components/NavBar'
+import NavBar from '../components/MenuNav'
 import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
